955ac34fc387f944e1693c1872b7e24dc5d623d0
TalhaQureshi324 <iamtalhaqureshi849@gmail.com>
2025-04-14 16:31:18 +0500

    Set up main entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,22 @@
+// import 'package:flutter/material.dart';
+// import 'screens/login_page.dart'; // Import LoginScreen
+
+// void main() {
+//   runApp(const CarRentalApp());
+// }
+
+// class CarRentalApp extends StatelessWidget {
+//   const CarRentalApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: const LoginScreen(), // Start from LoginScreen
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'screens/login_page.dart'; // Import LoginScreen
 
@@ -13,6 +32,11 @@ class CarRentalApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: const LoginScreen(), // Start from LoginScreen
+      theme: ThemeData(
+        // You can define basic colors and other properties here without text styles
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
     );
   }
 }
