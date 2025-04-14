640e6fe96dda159bb084f2e7f53cc7d0e807185b
TalhaQureshi324 <iamtalhaqureshi849@gmail.com>
2025-04-14 16:27:23 +0500

    Add notification_page refactored code

--- lib/screens/notification_page.dart
+++ lib/screens/notification_page.dart
@@ -1,3 +1,19 @@
+// import 'package:flutter/material.dart';
+
+// class NotificationsPage extends StatelessWidget {
+//   const NotificationsPage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("Notifications")),
+//       body: Center(
+//         child: Text("No new notifications!", style: TextStyle(fontSize: 18, color: Colors.grey)),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 class NotificationsPage extends StatelessWidget {
@@ -5,10 +21,18 @@ class NotificationsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Notifications")),
-      body: Center(
-        child: Text("No new notifications!", style: TextStyle(fontSize: 18, color: Colors.grey)),
+    return Scaffold(appBar: buildAppBar(), body: buildNotificationBody());
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(title: const Text("Notifications"));
+  }
+
+  Widget buildNotificationBody() {
+    return const Center(
+      child: Text(
+        "No new notifications!",
+        style: TextStyle(fontSize: 18, color: Colors.grey),
       ),
     );
   }
