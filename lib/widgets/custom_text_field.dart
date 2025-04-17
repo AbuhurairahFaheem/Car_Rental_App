8f9a1ffd527184afee2932af5d7aee894ea8696b
AbuhurairahFaheem <abuhurairahcs18@gmail.com>
2025-04-18 00:21:13 +0500

    created custom widget "text field" under newly created directory "widgets" to make them reusable.
    "write once and use everywhere"

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+  final TextEditingController controller;
+  final TextInputType keyboardType;
+  final bool obscureText;
+  final String? Function(String?) validator;
+
+  const CustomTextField({
+    super.key,
+    required this.label,
+    required this.controller,
+    required this.validator,
+    this.keyboardType = TextInputType.text,
+    this.obscureText = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      decoration: InputDecoration(
+        labelText: label,
+        border: OutlineInputBorder(),
+      ),
+      validator: validator,
+    );
+  }
+}
